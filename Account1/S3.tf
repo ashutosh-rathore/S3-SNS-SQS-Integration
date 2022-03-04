8c8f5e7ced56e6bc8374acfdfddedd216c70560e
Ashutosh Rathore <ashutosh@Ashutoshs-MacBook-Air.local>
2022-03-04 18:14:18 +0530

    s3 notifications

--- Account1/S3.tf
+++ Account1/S3.tf
@@ -2,32 +2,6 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "test-source-bucket-287444288085"
 }
 
-data "aws_caller_identity" "current" {}
-
-data "aws_iam_policy_document" "default" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "s3:PutObject",
-    ]
-
-    resources = [
-      "${aws_s3_bucket.bucket.arn}/*",
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "default" {
-  bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.default.json
-}
-
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.bucket.id
   topic {
@@ -36,7 +10,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   }
   depends_on = [
     aws_sns_topic.new-sns,
-    aws_s3_bucket_policy.default,
     aws_sns_topic_policy.default
   ]
 }
